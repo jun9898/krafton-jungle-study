61bead066d676ffa0c38c00fb970ff5e9efa282a
jun9898 <nice1998@gmail.com>
2023-05-20 22:18:32 +0900

    문제 해결 완료

--- 2305/230520/baekjoon1158.py
+++ 2305/230520/baekjoon1158.py
@@ -3,27 +3,21 @@ input = sys.stdin.readline
 
 # deque 말고 수열로 풀어봤음
 
-n, k = input().split()
+n, k = map(int,input().split())
 intn = int(n)
 intk = int(k) -1
-intktest = int(k) -1
+intktest = intk
 listTest = [i for i in range(1, intn+1)]
 result = []
-print(listTest)
 
-lenlist = len(listTest)
-
-
-while True:
-    if intk <= len(listTest):
-        result.append(listTest.pop(intk))
-        intk += intktest
-    elif len(listTest) == 0:
-        break
-    elif intk > len(listTest):
-        intk = intk % len(listTest)
-        continue
-        result.append(listTest.pop(intk))
+while listTest:
+    result.append(listTest.pop(intk))
+    intk += intktest
+    while listTest:
+        if (intk >= len(listTest)):
+            intk = intk % len(listTest)
+        else:
+            break
 
 print(str(result).replace("[", "<").replace("]", ">"))
 
