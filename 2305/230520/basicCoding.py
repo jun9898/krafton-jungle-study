a98e2b3369308b44c4af6e827df95c4b37787975
jun9898 <nice1998@gmail.com>
2023-05-21 12:04:32 +0900

    재귀 함수를 이용해 피보나치의 수열을 재현하였지만 재귀 함수의 실행시간이 너무나도 오래 나왔고 좀 더 코드를 효율적으로 작성해 피보나치 수열을 구현하는 코드를 짜보았다.

--- 2305/230520/basicCoding.py
+++ 2305/230520/basicCoding.py
@@ -21,24 +21,26 @@
 
 # 팩토리얼 함수
 
-def fac(n):
-    if n <= 1:
-        return 1
-    else:
-        return n*fac(n-1)
+# def fac(n):
+#     if n <= 1:
+#         return 1
+#     else:
+#         return n*fac(n-1)
 
-n = int(input("n : "))
-print(fac(n))
+# n = int(input("n : "))
+# print(fac(n))
 # 재귀 함수의 개념과 작동 원리를 이해했지만 응용하려면 더 깊게 이해할 필요가 있을것같다.
 
 # 피보나치 함수
+import sys
+input = sys.stdin.readline
 
-# def fibo(n):
-#     if n <= 2:
-#         return 1
-#     else:
-#         return fibo(n-2)+fibo(n-1)
+def fibo(n):
+    if n <= 2:
+        return 1
+    else:
+        return fibo(n-2)+fibo(n-1)
         
 
-# n = int(input("n : "))
-# print(fibo(n))
+n = int(input())
+print(fibo(n))
