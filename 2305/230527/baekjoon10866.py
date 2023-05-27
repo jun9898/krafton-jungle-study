aa6236a57acf4e376ae6eb96befccfbe6fea6b1b
jun9898 <nice1998@gmail.com>
2023-05-27 13:44:27 +0900

    기본적인 deque문제이다

---
+++ 2305/230527/baekjoon10866.py
@@ -0,0 +1,42 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+result = deque()
+
+for i in range(n):
+    commend = list(map(str, input().split()))
+    if "push_front" in commend:
+        result.appendleft(commend[-1])
+    if "push_back" in commend:
+        result.append(commend[-1])
+    if "pop_front" in commend:
+        if len(result) != 0:
+            print(result.popleft())
+        else:
+            print(-1)
+    if "pop_back" in commend:
+        if len(result) != 0:
+            print(result.pop())
+        else:
+            print(-1)
+    if "size" in commend:
+        print(len(result))
+    if "empty" in commend:
+        if len(result) <= 0:
+            print(1)
+        else:
+            print(0)
+    if "front" in commend:
+        if len(result) <= 0:
+            print(-1)
+        else:
+            print(result[0])
+    if "back" in commend:
+        if len(result) <= 0:
+            print(-1)
+        else:
+            print(result[-1])
+
+# 기본적인 deque문제
