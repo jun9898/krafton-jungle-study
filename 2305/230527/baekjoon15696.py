5f43b4e414e30e45cf428c409e65f9716af15b11
jun9898 <nice1998@gmail.com>
2023-05-28 14:31:58 +0900

    정답 판정을 받았으나 풀이를 보고 풀었으니 다시 한번 풀어보자.

--- 2305/230527/baekjoon15696.py
+++
@@ -1,9 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-def chickinDistens(n, m):
-    for i in range(n):
-        downTownMap = []
-        downTownMap.append(input().split() for _ in range(i))
-        for j in range(n):
-            if downTownMap[i][j] == 1
