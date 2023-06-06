e6308b633508c4c108f22229e2ad6f3d9b8eace1
jun9898 <nice1998@gmail.com>
2023-06-06 20:53:00 +0900

    백준 문제풀이

---
+++ 2306/230606/baekjoon10808.py
@@ -0,0 +1,6 @@
+ap = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+string = input()
+
+for i in ap:
+    print(string.count(i),end=" ")
