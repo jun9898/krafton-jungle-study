a206b719521b8c81904949f0f985f633075b69b3
jun9898 <nice1998@gmail.com>
2023-06-12 22:46:17 +0900

    백준 문제들!

--- 2306/230606/baekjoon11050.py
+++ 2306/230606/baekjoon11050.py
@@ -1,10 +1,51 @@
-n, k = map(int, input().split())
+# n, k = map(int, input().split())
 
-def fac(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * fac(n-1)
+# def fac(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * fac(n-1)
     
 
-print(fac(n) // (fac(n-k) * fac(k)))
+# print(fac(n) // (fac(n-k) * fac(k)))
+
+# a= {1,2,3,4,5}
+# b= {2,3,6}
+
+# c = a&b
+# e = a-b
+# f = a^b
+
+# print(c&e^f)
+
+# import t
+# print(1)
+# time.sleep(1)
+# print(2)
+
+# a = 1
+# def test():
+#     global a
+#     print(a)
+#     a = 100
+#     print(a)
+#     b = 2
+#     return a+b
+# print(test())
+# print(a)
+
+# a = ()
+# a = a+(1,2)
+# print(a)
+
+# import random
+
+# print(random.randint(1))
+
+# a = {"name" : "naem", "2" : "two2", "3" : "three3"}
+# print(a["two"])
+
+n = 1
+while n<= 5:
+    print("priunt")
+    n += 2
