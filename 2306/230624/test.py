f099c7777e84a17a5d07f5e45b17e66483b39727
jun9898 <nice1998@gmail.com>
2023-08-10 17:48:19 +0900

    python test

---
+++ 2306/230624/test.py
@@ -0,0 +1,12 @@
+
+num = int(input())
+result = 0
+
+for i in range(num):
+    n = int(input())
+    result += n
+
+if result > int(num/2) :
+    print("Junhee is cute!")
+else :
+    print("Junhee is not cute!")
