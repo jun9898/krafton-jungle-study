0abc2344ecdac6e2ba7d4e40b882647c42a1fc92
jun9898 <nice1998@gmail.com>
2024-04-09 14:39:00 +0900

    DP_1463 1로 만들기

---
+++ 2404/240409/1463(1로 만들기).py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+dp_table = [0] * (n+1)
+
+for i in range(2, n + 1):
+    dp_table[i] = dp_table[i-1] + 1
+    if i % 2 == 0:
+        dp_table[i] = min(dp_table[i], dp_table[i//2]+1)
+    if i % 3 == 0:
+        dp_table[i] = min(dp_table[i], dp_table[i//3]+1)
+
+print(dp_table[n])
