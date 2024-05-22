87aac54a594d1023517a2054124053c085f0c4b9
jun9898 <nice1998@gmail.com>
2024-05-22 11:09:32 +0900

    백준 실버 문제풀이

---
+++ algorithm/2405/240522/15652(N과 M(4)).py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+result = []
+
+
+def dfs(cur_arr, start, n, m):
+    if len(cur_arr) == m:
+        result.append(cur_arr[:])
+        return
+    for i in range(start, n + 1):
+        cur_arr.append(i)
+        dfs(cur_arr, i, n, m)
+        cur_arr.pop()
+
+
+n, m = map(int, input().split())
+dfs([], 1, n, m)
+
+for seq in result:
+    print(' '.join(map(str, seq)))
