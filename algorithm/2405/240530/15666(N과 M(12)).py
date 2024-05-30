c29200a3d2d2c452b3862a2744940b6845d9bd4a
jun9898 <nice1998@gmail.com>
2024-05-30 16:27:14 +0900

    백준 실버 문제풀이

---
+++ algorithm/2405/240530/15666(N과 M(12)).py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+result = []
+
+
+def dfs(cur_arr, n, m):
+    if len(cur_arr) == m:
+        result.append(cur_arr[:])
+        return
+    for i in range(0, n):
+        if len(cur_arr) != 0 and arr[i] < cur_arr[-1]:
+            continue
+        cur_arr.append(arr[i])
+        dfs(cur_arr, n, m)
+        cur_arr.pop()
+
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr = list(set(arr))
+arr.sort()
+
+dfs([], len(arr), m)
+
+for seq in result:
+    print(' '.join(map(str, seq)))
