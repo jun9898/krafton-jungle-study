bff01e07d64a51026c56b16ba479aa45724a625e
jun9898 <nice1998@gmail.com>
2024-06-12 18:11:06 +0900

    백준 골드 문제풀이

---
+++ algorithm/2406/240612/1238(파티).py
@@ -0,0 +1,36 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+
+def dfs(start_node):
+    dist = [float('inf')] * (N + 1)
+    dist[start_node] = 0
+    q = []
+    heapq.heappush(q, (0, start_node))
+    while q:
+        weight, node = heapq.heappop(q)
+        if dist[node] >= weight:
+            for v, val in graph[node]:
+                if weight + val < dist[v]:
+                    dist[v] = weight + val
+                    heapq.heappush(q, (weight + val, v))
+    return dist
+
+
+N, M, X = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for i in range(M):
+    start, end, weight = map(int, input().split())
+    graph[start].append([end, weight])
+
+root_visited = dfs(X)
+root_visited[0] = 0
+
+for i in range(1, N+1):
+    if i != X:
+        cur_visited = dfs(i)
+        root_visited[i] += cur_visited[X]
+
+print(max(root_visited))
