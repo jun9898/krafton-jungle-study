227d4e477f5cb8f9043f50f3bd5bf3fb29afbc2c
jun9898 <nice1998@gmail.com>
2024-08-08 15:39:07 +0900

    백준 골드 문제풀이

---
+++ algorithm/2408/240808/2310(어드벤처 게임).py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+def dfs(cur, cost):
+    global visited
+    currentLocation = graph[cur]
+    currentType = currentLocation[0]
+    nextLocation = list(map(int, currentLocation[2:-1]))
+    if currentType == "L":
+        cost = max(cost, int(currentLocation[1]))
+    elif currentType == "T":
+        cost -= int(currentLocation[1])
+        if cost < 0: return False
+    if cur == n:
+        return True
+    visited.add(cur)
+    for nextLo in nextLocation:
+        if nextLo not in visited:
+            if dfs(nextLo, cost):
+                return True
+    visited.remove(cur)
+    return False
+
+
+while True:
+    n = int(input())
+    if n == 0: break
+    graph = [()]
+    for i in range(n):
+        graph.append((input().rstrip().split()))
+    visited = set()
+    if (dfs(1, 0)):
+        print("Yes")
+    else:
+        print("No")
+
+
