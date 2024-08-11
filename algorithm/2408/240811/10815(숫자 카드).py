1719fee1f7e58783c560c1584f9ddbb24ee92b74
jun9898 <nice1998@gmail.com>
2024-08-11 13:15:51 +0900

    백준 실버 문제풀이 ㅋㅋ

---
+++ algorithm/2408/240811/10815(숫자 카드).py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+card = set(map(int, input().split()))
+m = int(input())
+answer = list(map(int, input().split()))
+
+for i in answer:
+    if i in card:
+        print(1, end=" ")
+    else:
+        print(0, end=" ")
+
