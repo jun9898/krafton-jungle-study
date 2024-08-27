d0ead98fb2791fb2c7f690a3b6929073dba0fcf8
jun9898 <nice1998@gmail.com>
2024-08-27 11:10:47 +0900

    백준 실버 문제풀이

---
+++ algorithm/2408/240827/2193(이친수).py
@@ -0,0 +1,14 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+n = int(input())
+dp = [0] * (n + 1)
+dp[1] = 1
+
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[n])
+
