2887723c179bbe75a4241cd7122105161db6075e
jun9898 <nice1998@gmail.com>
2024-08-31 13:42:20 +0900

    백준 실버 문제풀이

---
+++ algorithm/2408/240831/15665(N과 M 11).py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def back_track(tmp_arr):
+    if len(tmp_arr) == m:
+        print(*tmp_arr)
+        return
+    for i in range(len(arr)):
+        tmp_arr.append(arr[i])
+        back_track(tmp_arr)
+        tmp_arr.pop()
+
+n, m = map(int, input().split())
+arr = sorted(dict.fromkeys(map(int, input().split())))
+
+back_track([])
+
+
+
